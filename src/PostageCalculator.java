d3c6f204a917840d77575a2760e015302ad17c57
BT_4N2_04 <zraykh2784@bths.edu>
2023-02-17 11:29:58 -0500

    Finished PostageCalculator.

--- src/PostageCalculator.java
+++ src/PostageCalculator.java
@@ -5,7 +5,7 @@ public class PostageCalculator {
     {
         double cost = BASECOST;
         cost += 0.05 * (lb * 10);
-        cost += Math.abs(Integer.parseInt(zip1.substring(0, 3)) -  Integer.parseInt(zip2.substring(0, 3))) /100;
+        cost += Math.abs(Integer.parseInt(zip1.substring(0, 3)) -  Integer.parseInt(zip2.substring(0, 3))) /100.0;
         double combinedInch = h + w + l;
         for (int i = 36; i < combinedInch; i++)
         {
@@ -13,19 +13,53 @@ public class PostageCalculator {
         }
         for (int i = 40; i < lb; i++)
         {
-            cost += 10;
+            cost += 0.1;
         }
         return cost;
     }
 
     public static double calculatePostage(Address a, Address b, double lb, double h, double w, double l)
     {
-        return a;
+        String zip1 = a.getZipCode();
+        String zip2 = b.getZipCode();
+        double cost = BASECOST;
+        cost += 0.05 * (lb * 10);
+        cost += Math.abs(Integer.parseInt(zip1.substring(0, 3)) -  Integer.parseInt(zip2.substring(0, 3))) /100.0;
+        double combinedInch = h + w + l;
+        for (int i = 36; i < combinedInch; i++)
+        {
+            cost += 0.1;
+        }
+        for (int i = 40; i < lb; i++)
+        {
+            cost += 0.1;
+        }
+        return cost;
     }
 
     public static double calculatePostage(Package p)
     {
-
+        Address a = p.getDestination();
+        Address b = p.getOrigin();
+        double  lb = p.getPounds();
+        double h = p.getHeightIn();
+        double w = p.getWidthIn();
+        double l = p.getLengthIn();
+        String zip1 = a.getZipCode();
+        String zip2 = b.getZipCode();
+        double cost = BASECOST;
+        cost += 0.05 * (lb * 10);
+        cost += Math.abs(Integer.parseInt(zip1.substring(0, 3)) -  Integer.parseInt(zip2.substring(0, 3))) /100.0;
+        double combinedInch = h + w + l;
+        for (int i = 36; i < combinedInch; i++)
+        {
+            cost += 0.1;
+        }
+        for (int i = 40; i < lb; i++)
+        {
+            cost += 0.1;
+        }
+        return cost;
     }
 }
 
