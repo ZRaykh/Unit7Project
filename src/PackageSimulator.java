5a50e736ad932fe1e941ee6ab27854a304f84780
ZRaykh <zraykh2784@bths.edu>
2023-02-27 20:18:05 -0500

    Began working on Package Simulator

---
+++ src/PackageSimulator.java
@@ -0,0 +1,22 @@
+import  java.util.ArrayList;
+import  java.text.DecimalFormat;
+public class PackageSimulator
+{
+    ArrayList<Package> p = new ArrayList<Package>();
+
+    public PackageSimulator() {}
+
+    public void generatePackages(int packages)
+    {
+
+    }
+
+    /*/public String generateZip()
+    {
+
+        DecimalFormat d = new DecimalFormat("00");
+        String zip = "";
+        zip += (int) (Math.random() * 10);
+        zip += d.format((int) (Math.random() * 100));
+    }/*/
+}
